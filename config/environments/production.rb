2ee53bae155533ef0eb84a22f76aba9234428cd3
John_Hayes-Reed <john.hayes.reed@gmail.com>
2016-10-30 15:03:56 +0900

    more gem additions, devise setup, home controller generation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "personal_homepage_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 } # TODO: change this to production host when releasing
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
