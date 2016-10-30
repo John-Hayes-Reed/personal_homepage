bac3fd0ddc32c63f34c405de68e9064cc276c03f
John_Hayes-Reed <john.hayes.reed@gmail.com>
2016-10-30 15:39:10 +0900

    added stuff for admin section, blogs, and started footer content

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 
+  devise_for :admins
   root to: 'home#index'
 
   resources :home, only: [:index]
   resources :blogs, only: [:index, :show]
+
+  namespace :admin do
+    resources :home, only: [:index]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
