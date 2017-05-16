ab47bcb75a86451859b98c473d132d600b3432e3
John Hayes-Reed <john.hayes.reed@gmail.com>
2017-05-17 01:31:07 +0900

    match create with update for project services.

--- app/services/create_project.rb
+++ app/services/create_project.rb
@@ -3,6 +3,15 @@ class CreateProject < BottledService
   att :title, String
   att :project_type, String
   att :url, String
+  att :server, String
+  att :database, String
+  att :backend, String
+  att :frontend, String
+  att :templates, String
+  att :app, String
+  att :version_control, String
+  att :language, String
+  att :other, String
   att :description, String
 
 
@@ -11,6 +20,15 @@ class CreateProject < BottledService
       title: @title,
       url: @url,
       project_type: @project_type,
+      server: @server,
+      database: @database,
+      backend: @backend,
+      frontend: @frontend,
+      templates: @templates,
+      app: @app,
+      version_control: @version_control,
+      language: @language,
+      other: @other,
       description: @description
     )
     if @project.errors.present?
