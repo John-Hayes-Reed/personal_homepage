40a57fb09b88ecff0e3e2fb5548c9d8d24d72a47
John Hayes-Reed <john.hayes.reed@gmail.com>
2017-05-20 12:34:07 +0900

    Initialize projects service and specs

---
+++ app/services/filter_project_params.rb
@@ -0,0 +1,32 @@
+# @abstract A Service Object for handling the Strong Parameters filtering for
+#   Project model mass assignment.
+class FilterProjectParams < BottledService
+  att :params
+
+  # Executes the Service Object.
+  #
+  # @raise [ActionController::ParameterNotFound] if the required key is not
+  #   present.
+  # @raise [ActionController::UnpermittedParameter] if a parameter not in the
+  #   whitelist is present.
+  #
+  # @return [ActionController::Parameters]
+  def call
+    @params.require(:project).permit(*allowed_params)
+  end
+
+  private
+
+  # Provides a list of the allowed parameters for Project writes.
+  #
+  # @return [Array]
+  def allowed_params
+    %i[
+      id
+      title
+      project_type
+      url
+      description
+    ].push(*Project::TECHNOLOGY_ATTRIBUTES)
+  end
+end
--- spec/services/filter_project_params_spec.rb
+++ spec/services/filter_project_params_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
 
 describe FilterProjectParams do
+  subject { described_class.call params: params }
 
+  context 'with valid parameters' do
+    let(:params) do
+      prms = ActionController::Parameters.new(
+        project: attributes_for(:project)
+      )
+      prms[:project].delete('technologies')
+      Project::TECHNOLOGY_ATTRIBUTES.each do |att|
+        prms[:project][att] = 'a'
+      end
+      prms
+    end
+
+    it { is_expected.to be_a ActionController::Parameters }
+    %i[title project_type url description]
+      .push(*Project::TECHNOLOGY_ATTRIBUTES)
+      .each do |key|
+
+      it { is_expected.to include key.to_s }
+    end
+  end
+
+  context 'with invalid parameters' do
+    let(:params) do
+      ActionController::Parameters.new(
+        project: attributes_for(:project).merge(invalid: true)
+      )
+    end
+
+    it do
+      is_expected.to(satisfy { |res| !res.keys.map(&:to_s).include? 'invalid' })
+    end
+  end
+
+  context 'with incorrect key' do
+    let(:params) do
+      ActionController::Parameters.new(
+        illegal: attributes_for(:project)
+      )
+    end
+    it { expect { subject }.to raise_error ActionController::ParameterMissing }
+  end
 end
